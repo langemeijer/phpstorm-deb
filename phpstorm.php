d7afc6bcb2411ff786da8d1d5bef6f02b0ad4453
Casper Langemeijer <casper.langemeijer@procurios.nl>
2018-02-19 14:32:07 +0100

    Add support for phpstorm:// url's

---
+++ phpstorm.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php
+<?php
+
+file_put_contents('/tmp/phpstorm.commandline', $argv[1]);
+
+if ($argc === 2 && preg_match('(^phpstorm:\/\/open\?file=(.*)\&line=([0-9]+)\/?$)', $argv[1], $matches)) {
+
+	$fileName = escapeshellarg($matches[1]);
+	$lineNumber = escapeshellarg($matches[2]);
+
+	system("/opt/PhpStorm/bin/phpstorm.sh --line " . $lineNumber . " " . $fileName);
+} else {
+
+	array_shift($argv);
+	$cmdLine = implode(" ", array_map('escapeshellarg', $argv));
+
+	system("/opt/PhpStorm/bin/phpstorm.sh " . $cmdLine);
+}
+
