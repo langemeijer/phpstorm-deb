d1da43359c6741543cb6d75f9e111988081c40b5
Ewan Thompson <ewanpt@gmail.com>
2025-06-05 20:55:44 +0800

    Use native launcher.
    PhpStorm advises to 'Switch to a native launcher' if launched from a script like 'phpstorm.sh', advising to use the native binary instead.

--- phpstorm.php
+++ phpstorm.php
@@ -8,12 +8,12 @@ if ($argc === 2 && preg_match('(^phpstorm:\/\/open\?file=(.*)\&line=([0-9]+)\/?$
 	$fileName = escapeshellarg(urldecode($matches[1]));
 	$lineNumber = escapeshellarg(urldecode($matches[2]));
 
-	system("/opt/PhpStorm/bin/phpstorm.sh --line " . $lineNumber . " " . $fileName);
+	system("/opt/PhpStorm/bin/phpstorm --line " . $lineNumber . " " . $fileName);
 } else {
 
 	array_shift($argv);
 	$cmdLine = implode(" ", array_map('escapeshellarg', $argv));
 
-	system("/opt/PhpStorm/bin/phpstorm.sh " . $cmdLine);
+	system("/opt/PhpStorm/bin/phpstorm " . $cmdLine);
 }
 
